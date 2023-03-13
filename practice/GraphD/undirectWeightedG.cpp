273c2e952fc01b8cdcd5c617a873e90787342b6c
MSI75 <sisaif56@gmail.com>
2023-03-13 22:49:05 +0600

    comment

--- practice/GraphD/undirectWeightedG.cpp
+++ practice/GraphD/undirectWeightedG.cpp
@@ -1,29 +1,29 @@
 // undirect + weighted Graph
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-int main()
-{
-    int nodes;
-    cin >> nodes;
-    int adjMatrix[nodes][nodes] = {};
+// int main()
+// {
+//     int nodes;
+//     cin >> nodes;
+//     int adjMatrix[nodes][nodes] = {};
 
-    int edge;
-    cin >> edge;
-    for (int i = 0; i < edge; i++)
-    {
-        int u, v, w;
-        cin >> u >> v >> w;
-        adjMatrix[u][v] = w;
-        adjMatrix[v][u] = w;
-    }
+//     int edge;
+//     cin >> edge;
+//     for (int i = 0; i < edge; i++)
+//     {
+//         int u, v, w;
+//         cin >> u >> v >> w;
+//         adjMatrix[u][v] = w;
+//         adjMatrix[v][u] = w;
+//     }
 
-    for (int i = 0; i < nodes; i++)
-    {
-        for (int j = 0; j < nodes; j++)
-        {
-            cout << adjMatrix[i][j] << " ";
-        }
-        cout << "\n";
-    }
-}
+//     for (int i = 0; i < nodes; i++)
+//     {
+//         for (int j = 0; j < nodes; j++)
+//         {
+//             cout << adjMatrix[i][j] << " ";
+//         }
+//         cout << "\n";
+//     }
+// }
