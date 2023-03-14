08d33982b69078e5041d716f976a6964a4c40324
MSI75 <sisaif56@gmail.com>
2023-03-14 22:00:30 +0600

    modify

--- Algorithm/Modul_5_5P/recursion.cpp
+++ Algorithm/Modul_5_5P/recursion.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void reverseString(string s, int i)
+void reverse(string str, int i)
 {
-    int r = s.size();
+    int r = str.size();
     // base case
     if (i > r)
         return;
     // generalized formula
-    char ch = s[i];
+    char ch = str[i];
     i = i + 1;
-    reverseString(s, i);
+    reverse(str, i);
 
     cout << ch;
 }
@@ -19,7 +19,7 @@ int main()
 {
     string s;
     cin >> s;
-    reverseString(s, 0);
+    reverse(s, 0);
     cout << "\n";
     return 0;
 }
