00d76806c3c2cdd3917bddca16123bf02b1d9b13
MSI75 <sisaif56@gmail.com>
2023-03-15 10:06:33 +0600

    DFS

---
+++ Algorithm/Modul_6Lab/DFS.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5;
+
+int visited[N];
+
+vector<int> adj_list[N];
+
+void DFS(int node)
+{
+    cout << node << " ";
+
+    visited[node] = 1;
+
+    for (int adj_node : adj_list[node])
+    {
+        if (visited[adj_node] == 0)
+            DFS(adj_node);
+    }
+}
+
+int main()
+{
+    int node, edge;
+    cin >> node >> edge;
+
+    for (int i = 0; i < edge; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        adj_list[u].push_back(v);
+        adj_list[v].push_back(u);
+    }
+
+    int src = 0;
+    DFS(src);
+    return 0;
+}
+
+/*
+6 6
+
+0 1
+1 2
+1 5
+2 3
+2 4
+3 5
+*/
