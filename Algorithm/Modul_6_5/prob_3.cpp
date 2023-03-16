e97e1c2b75707678173c0ebf970fceeaa56117d3
MSI75 <sisaif56@gmail.com>
2023-03-16 12:49:04 +0600

    prob_3

---
+++ Algorithm/Modul_6_5/prob_3.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5;
+int visited[N];
+vector<int> adj_list[N];
+int checked[N];
+
+void BFS(int src)
+{
+    queue<int> q;
+    visited[src] = 1;
+    q.push(src);
+
+    while (!q.empty())
+    {
+        int head = q.front();
+        q.pop();
+
+        for (auto child : adj_list[head])
+        {
+            if (visited[child] == 0)
+            {
+                visited[child] = 1;
+                q.push(child);
+            }
+            checked[child]++;
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    for (int i = 0; i < e; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj_list[u].push_back(v);
+        adj_list[v].push_back(u);
+    }
+    cout<<"Adjecency List : \n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << " -> ";
+        for (auto list : adj_list[i])
+            cout << list << " ";
+        cout << "\n";
+    }
+
+    cout<<"Cheaked List : \n";
+    BFS(0);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << " -> " << checked[i] << "\n";
+    }
+}
