cc9278b9d98f6b46aaab2b4768b91cbe27020289
MSI75 <sisaif56@gmail.com>
2023-04-27 20:20:14 +0600

    fibo tabulation

--- AlgorithmDP/Modul_23LabAssignment_2/fibo_memorization.cpp
+++ AlgorithmDP/Modul_23LabAssignment_2/fibo_memorization.cpp
@@ -6,11 +6,8 @@ int dp[N];
 
 int fib(int n)
 {
-    if (n == 2 || n == 1)
-        return 1;
-    else if (n <= 0)
-        return 0;
-
+    if (n <= 1)
+        return n;
 
     if (dp[n] != -1)
         return dp[n];
